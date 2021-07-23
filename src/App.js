8878fbb990c96904aee6f92702f1284395d3106a
leeseungjin <dltmdwls154@gmail.com>
2021-07-23 15:26:49 +0900

    news item, list 구현

--- src/App.js
+++ src/App.js
@@ -1,22 +1,8 @@
-import React,{useState} from 'react'
-import axios from 'axios'
+import React from 'react';
+import NewsList from './components/NewsList';
 
 function App() {
-  const [data, setData] = useState(null);
-  const onClick = () => {
-    axios.get('https://jsonplaceholder.typicode.com/todos/1').then(response => {
-      setData(response.data);
-    })};
-  return (
-    <div>
-      <div><button onClick={onClick}>불러오기</button></div>
-      <div>
-        {data && <textarea rows={7} value={JSON.stringify(data,null,2)} readOnly={true} />}
-      </div>
-    </div>
-  )
+  return <NewsList />;
 }
 
 export default App;
-
-// 'apikey = a3d94194042b4e1eb6dbea2fe877d1e5'
